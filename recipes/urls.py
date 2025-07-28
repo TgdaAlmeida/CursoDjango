608b2e837c5f5c6395c5ed6371067124245afbe2
Thiago Gomes de Almeida <thiagotgda2@gmail.com>
2025-07-28 20:17:41 -0300

    url

--- recipes/urls.py
+++ recipes/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 #dominio.com/recipes/
 urlpatterns = [
-    path("", views.home), #HomePage
-    path("recipes/<int:id>/", views.recipe),
+    path("", views.home, name="recipes-home"), #HomePage
+    path("recipes/<int:id>/", views.recipe, name="recipes-recipe"),
 ]
