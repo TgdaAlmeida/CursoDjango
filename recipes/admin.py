098e6b9c893817243abd5109ab97676cd35a1b38
Thiago Gomes de Almeida <thiagotgda2@gmail.com>
2025-07-31 10:48:42 -0300

    djangoadmin

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
+from .models import Category, Recipe
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    ...
+
+@admin.register(Recipe)
+
+class RecipeAdmin(admin.ModelAdmin):
+    ...
+
+
+#admin.site.register( Recipe, RecipeAdmin)
+
+
+admin.site.register( Category, CategoryAdmin)
+
 # Register your models here.
