fac9c112fe1a7ed6e1f6e22dd6c6c76dc640c7ee
Thiago Gomes de Almeida <thiagotgda2@gmail.com>
2025-08-07 13:57:51 -0300

    django

--- recipes/urls.py
+++ recipes/urls.py
@@ -7,5 +7,6 @@ app_name = 'recipes'
 #dominio.com/recipes/
 urlpatterns = [
     path("", views.home, name="home"), #HomePage
+    path('recipes/category/<int:category_id>/',views.category, name="category"),
     path("recipes/<int:id>/", views.recipe, name="recipe"),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,11 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import get_list_or_404, render
 
+from recipes.models import Recipe
 from utils.recipes.factory2 import make_recipe
 
 
 def home(request):
+    recipes = Recipe.objects.filter(is_published=True).order_by('-id')
     return render(request,'recipes/pages/home.html', context={
-        'recipes': [make_recipe() for _ in range(10)],
+        'recipes': recipes,
+    })
+
+def category(request, category_id):
+    recipes = get_list_or_404(Recipe.objects.filter(category__id=category_id, is_published=True,).order_by('-id'))
+  
+    return render(request,'recipes/pages/category.html', context={
+        'recipes': recipes,
+        'title': f'{recipes[0].category.name} - Category | '
     })
 
 def recipe(request, id):
